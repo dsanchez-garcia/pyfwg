cab89733ccd21f2831e363f42f98f734aeeb9e95
Daniel Sánchez-García <daniel.sanchezgarcia@uca.es>
2025-09-22 21:10:53 +0200

    v0.2.0

--- using morph_epw.py
+++ using morph_epw.py
@@ -8,12 +8,12 @@ created_files_custom = morph_epw_global(
     epw_paths=epw_file,
     fwg_jar_path=jar_path,
     output_dir='./custom_output',
-    fwg_show_tool_output=True,
+    fwg_show_tool_output=False,
     delete_temp_files=False,
     fwg_gcms=['CanESM5'], # Usar solo dos GCMs
     fwg_interpolation_method_id=2,  # Usar el método "nearest point"
     fwg_epw_original_lcz=2,
-    fwg_target_uhi_lcz=3
+    fwg_target_uhi_lcz=3,
 )
 
 # print("Successfully created files:")
@@ -43,3 +43,27 @@ created_files_custom = morph_epw_europe(
 # print("Successfully created files:")
 # for f in created_files_custom:
 #     print(f)
+
+##
+import os
+from pyfwg import morph_epw_global
+
+# --- Configuration ---
+# Define the path to the EPW file to be processed. You can also define a list of paths to process multiple EPW files.
+epw_file = 'epws/w_pattern/sevilla_uhi-type-1.epw'
+
+# !!! IMPORTANT: You MUST change this path to the correct location on your PC !!!
+jar_path = r"D:\OneDrive - Universidad de Cádiz (uca.es)\Programas\FutureWeatherGenerator_v3.0.1.jar"
+# The validation of parameters is executed automatically inside the function.
+created_files_custom = morph_epw_global(
+    epw_paths=epw_file,
+    fwg_jar_path=jar_path,
+    output_dir='./custom_output_global',
+    fwg_show_tool_output=True,      # See the tool's progress in real-time
+    fwg_gcms=['CanESM5'],           # Use only one specific GCM
+    fwg_interpolation_method_id=2 # Use the "nearest point" method
+)
+
+print("\nSuccessfully created files:")
+for f in created_files_custom:
+    print(f)
