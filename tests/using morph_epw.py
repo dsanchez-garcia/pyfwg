f0b4df302d351d841722cce50f3a046abbb0c861
Daniel Sánchez-García <daniel.sanchezgarcia@uca.es>
2025-12-29 20:31:20 +0100

    test: Add functional tests for `morph_epw_global` and `morph_epw_europe` high-level API functions.

--- tests/using morph_epw.py
+++ tests/using morph_epw.py
@@ -34,13 +34,15 @@ created_files_custom = morph_epw_global(
 from pyfwg import morph_epw_europe
 
 jar_path = r"D:\OneDrive - Universidad de Cádiz (uca.es)\Programas\FutureWeatherGenerator_Europe_v1.0.1.jar"
-epw_file = 'MAD_ICU_type-2.epw'
+# jar_path = r"D:\OneDrive - Universidad de Cádiz (uca.es)\Programas\FutureWeatherGenerator_Europe_v2.0.2.jar"
+
+epw_file = 'epws/wo_pattern/GBR_London.Gatwick.037760_IWEC_uhi_type-2.epw'
 
 # La validación se ejecuta automáticamente.
 created_files_custom = morph_epw_europe(
     epw_paths=epw_file,
     fwg_jar_path=jar_path,
-    output_dir='./custom_output',
+    output_dir='./custom_output_europe_v1',
     fwg_show_tool_output=True,
     delete_temp_files=False,
     fwg_rcm_pairs=['ICHEC_EC_EARTH_SMHI_RCA4'],
@@ -61,16 +63,18 @@ from pyfwg import morph_epw_global
 # Define the path to the EPW file to be processed. You can also define a list of paths to process multiple EPW files.
 # epw_file = 'epws/w_pattern/sevilla_uhi-type-1.epw'
 # epw_file = 'pyfwg/tutorials/epws/wo_pattern/GBR_London.Gatwick.037760_IWEC_uhi_type-2.epw'
-epw_file = r'pyfwg/tutorials/epws/wo_pattern/GBR_London.Gatwick.037760_IWEC_uhi_type-2.epw'
+epw_file = 'epws/wo_pattern/GBR_London.Gatwick.037760_IWEC_uhi_type-2.epw'
 
 
 # !!! IMPORTANT: You MUST change this path to the correct location on your PC !!!
 jar_path = r"D:\OneDrive - Universidad de Cádiz (uca.es)\Programas\FutureWeatherGenerator_v3.0.1.jar"
+# jar_path = r"D:\OneDrive - Universidad de Cádiz (uca.es)\Programas\FutureWeatherGenerator_v4.0.2.jar"
+
 # The validation of parameters is executed automatically inside the function.
 created_files_custom = morph_epw_global(
     epw_paths=epw_file,
     fwg_jar_path=jar_path,
-    output_dir='./custom_output',
+    output_dir='./custom_output_global_v3',
     fwg_show_tool_output=True,
     delete_temp_files=False,
     fwg_gcms=['CanESM5'], # Usar solo dos GCMs
